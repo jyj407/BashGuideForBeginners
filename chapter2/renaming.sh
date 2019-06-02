c8e31884c95fdfa1b6c877fb0998799d5e3119e0
Tony <yanjunjiangjn@gmail.com>
2019-06-02 10:27:36 -0400

    [Chapter2] Renaming files without using rename command.

--- chapter2/renaming.sh
+++ chapter2/renaming.sh
@@ -3,6 +3,8 @@
 # Desc: Rename jpg and png files
 
 count=1
+# Note here -iname is similar to -name, but ignore cases.
+# So we can match suffixes like: png, jpg, PNG, JPG
 for img in `find -iname '*.png' -o -iname '*.jpg' -type f -maxdepth 1`
 do
 	new=image-$count.${img##*.}
